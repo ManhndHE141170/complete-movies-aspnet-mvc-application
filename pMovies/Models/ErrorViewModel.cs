64fcac52aeece0df44c4fa513e7782575106dbe4
ManhndHE141170 <Manhndhe141170@fpt.edu.vn>
2022-07-21 21:31:09 +0700

    Add project files.

---
+++ pMovies/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace pMovies.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
